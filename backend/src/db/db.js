0b34e6b952c20d3c7f95c23f3274836a796df8be
Akshay Jalluri <141540026+akshayjalluri6@users.noreply.github.com>
2024-10-07 22:56:00 +0530

    Update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,9 +2,9 @@ import pkg from 'pg'
 const {Pool} = pkg
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'parker',
-    password: 'postgres',
-    port: 5434
+    user: '',
+    host: '',
+    database: '',
+    password: '',
+    port: 
 })
